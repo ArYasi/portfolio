edc5f3eaa1a5df3526238772e0ef5e8e3313142d
Artur Yasinskiy <artur@MPro14.local>
2022-11-15 19:10:42 -0800

    Header updated. finished for now

--- components/header.js
+++ components/header.js
@@ -11,20 +11,20 @@ const Header = () => {
     
     return(
         <div className="flex justify-center">
-            <nav className="p-7 md:w-5/6 md:flex md:items-center md:justify-between">
+            <nav className="p-7 md:w-5/6 md:flex md:items-center md:justify-between border-green-300 border-4 rounded-lg m-5 shadow-[6px_6px_0px_0px_rgba(239,68,68)]">
                 <div>
                     <span className="text-header text-white font-term">ARTUR YASINSKIY</span>
                 </div>
 
                 <ul className="md:flex md:items-center hidden">
                     <li className="mx-4">
-                        <a href="#" className="font-term text-header text-white hover:text-green-200" ref={navEffect.ref}>ABOUT ME</a>
+                        <a href="#" className="font-term text-header text-white hover:text-green-300" ref={navEffect.ref}>ABOUT ME</a>
                     </li>
                     <li className="mx-4">
-                        <a href="#" className="font-term text-header text-white hover:text-green-200" ref={navEffect.ref}>PROJECTS</a>
+                        <a href="#" className="font-term text-header text-white hover:text-green-300" ref={navEffect.ref}>PROJECTS</a>
                     </li>
                     <li className="mx-4">
-                        <a href="#" className="font-term text-header text-white hover:text-green-200" ref={navEffect.ref}>CONTACT</a>
+                        <a href="#" className="font-term text-header text-white hover:text-green-300" ref={navEffect.ref}>CONTACT</a>
                     </li>
                 </ul>
             </nav>
